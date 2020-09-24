a7d8a25ee14fadf8527142ef1e95ad4eab179d3c
Cooper Hollmaier <cooperjhollmaier@gmail.com>
2020-09-23 18:26:37 -0700

    Building and returning phrases

--- js/Game.js
+++ js/Game.js
@@ -5,8 +5,22 @@
 class Game {
     constructor() {
         this.missed = 0;
-        this.phrases = [];
+        this.phrases = this.createPhrases();
         this.activePhrase = null;
     }
-    createPhrases() {};
+    createPhrases() {
+        const phrases = [
+            { phrase: 'This is the first phrase' },
+            { phrase: 'This is the second phrase' },
+            { phrase: 'This is the third phrase' },
+            { phrase: 'This is the fourth phrase' },
+            { phrase: 'This is the fifth phrase' }
+        ];
+        return phrases;
+    };
+
+    randomPhrase() {
+
+
+    };
 }
--- js/app.js
+++ js/app.js
@@ -6,4 +6,5 @@ const game = new Game();
 
 game.phrases.forEach((phrase, index) => {
     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
+
 });
